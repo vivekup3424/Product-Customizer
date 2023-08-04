a97a11878ccc9e19a376fcb7a07497d7c08d9815
vivekup3424 <vivek.hygfrt324@gmail.com>
2023-08-04 12:09:41 +0530

    set up the frontend, not set up backend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,52 +1,62 @@
-//make a home page
-import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
+
 import state from "../store";
+import { CustomButton } from "../components";
 import {
-    headContentAnimation,
     headContainerAnimation,
+    headContentAnimation,
     headTextAnimation,
     slideAnimation,
 } from "../config/motion";
-export default function Home() {
-    const snap = useSnapshot(state); //state management
+
+const Home = () => {
+    const snap = useSnapshot(state);
+
     return (
         <AnimatePresence>
-            {/* if on homepage render homepage data */}
             {snap.intro && (
                 <motion.section className="home" {...slideAnimation("left")}>
                     <motion.header {...slideAnimation("down")}>
                         <img
-                            src="../public/threejs.png"
+                            src="./threejs.png"
                             alt="logo"
                             className="w-8 h-8 object-contain"
-                        ></img>
+                        />
                     </motion.header>
+
                     <motion.div
                         className="home-content"
                         {...headContainerAnimation}
                     >
                         <motion.div {...headTextAnimation}>
                             <h1 className="head-text">
-                                Let's <br className="xl:block hidden" />
+                                LET'S <br className="xl:block hidden" /> DO IT.
                             </h1>
                         </motion.div>
                         <motion.div
                             {...headContentAnimation}
                             className="flex flex-col gap-5"
                         >
-                            <p className="max-w-md font-normal text-base text-gray-600">
-                                Create your unique and exclusive shirt with your
-                                brand-new 3D customization tool.
-                                <br className="xl:block hidden" />
-                                <strong>Unleash Your Imagination</strong> and
-                                define your style.
+                            <p className="max-w-md font-normal text-gray-600 text-base">
+                                Create your unique and exclusive shirt with our
+                                brand-new 3D customization tool.{" "}
+                                <strong>Unleash your imagination</strong> and
+                                define your own style.
                             </p>
+
+                            <CustomButton
+                                type="filled"
+                                title="Customize It"
+                                handleClick={() => (state.intro = false)}
+                                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                            />
                         </motion.div>
                     </motion.div>
                 </motion.section>
             )}
         </AnimatePresence>
     );
-}
+};
+
+export default Home;
