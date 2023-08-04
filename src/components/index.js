9dada261be9ee88fe934e5b93062545908ce2fd9
vivekup3424 <vivek.hygfrt324@gmail.com>
2023-08-04 12:06:28 +0530

    update the component and set up the fronend

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+//import all components and export them
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab };
