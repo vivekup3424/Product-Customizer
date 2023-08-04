37892b59b2a8b8673cc5bd40df71dc1ac605eed7
vivekup3424 <vivek.hygfrt324@gmail.com>
2023-08-04 12:14:29 +0530

    start the backend server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json({ limig: "50mb" }));
+
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello from DALL.E" });
+});
+
+app.listen(8080, () => console.log("Server has started on port 8080"));
