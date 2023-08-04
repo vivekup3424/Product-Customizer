d97e7bd63668dd0221383297ca25ea2381766888
vivekup3424 <vivek.hygfrt324@gmail.com>
2023-08-04 16:28:47 +0530

    fixed typo in other  page

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
-import { easing } from "maath";
-import { useFrame } from "@react-three/fiber";
+//import { easing } from 'maath';
+//import { useFrame } from '@react-three/fiber';
 import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
 const Backdrop = () => {
